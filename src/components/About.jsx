e6da39e6f0d7e0a15389d40f290794491cf29c1c
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2025-11-15 20:23:49 +0530

    adding fils

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export default function About() {
 return (
 <section id="about" className="mt-16 bg-white rounded-2xl p-8 shadow-sm">
-<h3 className="text-2xl font-semibold flex items-center gap-3">About Me <a href="/MYRESUME.pdf" target="_blank" rel="noreferrer" className="px-3 py-1 border rounded-md text-sm bg-gradient-to-r from-yellow-300 to-yellow-400 text-black">Resume</a></h3> 
+<h3 className="text-2xl font-semibold flex items-center gap-3">About Me <a href="/updated_resume.pdf" target="_blank" rel="noreferrer" className="px-3 py-1 border rounded-md text-sm bg-gradient-to-r from-yellow-300 to-yellow-400 text-black">Resume</a></h3> 
 <p className="mt-4 text-gray-600">Hello, I'm Nirupam a final year Computer Science Undergrad at Jorhat Engineering College. I love to build  web applications, solve problems, learn about cloud services etc. </p>
 <div className="mt-6 grid md:grid-cols-3 gap-4">
 <div className="p-4 border rounded-lg">
